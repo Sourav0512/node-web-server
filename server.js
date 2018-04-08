d89ce2077840b7441c1c9662a181478d699514f0
Sourav Das <05sourav@gmail.com>
2018-04-08 15:50:15 +0530

    commit server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 console.log('server.js fired up');
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 var app = express();
 // app.get('/',(request,response)=> response.send({name : 'Sourav Das' , age : 27 , profession : 'Software'}));
 
@@ -8,16 +9,24 @@ app.use(express.static(__dirname + '/public')); // dir to load static content fr
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine',hbs); // setting the view engine to HBS
 hbs.registerHelper('getCapital',(text)=> text.toUpperCase());
-app.get('/about',(request,response) =>{
-                                      response.render('about.hbs',{Title : 'About Title' ,Para : 'About Page'
-                                      ,FooterData : `Copyright by Sourav Das @ ${(new Date()).getFullYear()}`
-                                      })
-                                      });
+hbs.registerHelper('getTimeStamp',()=> (new Date()));
 
-app.get('/',(request,response) =>    {
-                                      response.render('home.hbs',{Title : 'Home Page', Para : 'some random body here',                                      ra : 'Some Body here',
-                                      FooterData : `Copyright by Sourav Das @ ${(new Date()).getFullYear()}`
-                                      })
-                                      });
+ //middleware for Maintenance page
+app.use((request,response,next)=>{
+  response.render('maintenance.hbs',{
+    Title : 'sorry for the inconvinience' ,
+    maintain : 'maintenance going on'
+  });
+next();
+});
+
+app.use((request,response,next)=> {
+  fs.appendFile('server.log',`${(new Date())} :  ${request.method} : ${request.url} ` + '\n'
+                              ,(error) => {if(error) console.log('error occured')});
+  next();
+});
+app.get('/about',(request,response) =>{response.render('about.hbs',{Title : 'About Title' ,Para : 'About Page'})});
+
+app.get('/',(request,response) =>{response.render('home.hbs',{Title : 'Home Page', Para : 'some random body here'})});
 
 app.listen(4000);
