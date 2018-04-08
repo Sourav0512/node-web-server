c6b0a093bc099cb9c36aabac49b7ca498a2431a4
Sourav Das <05sourav@gmail.com>
2018-04-08 14:58:09 +0530

    commit partials and helpers

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ const hbs = require('hbs');
 var app = express();
 // app.get('/',(request,response)=> response.send({name : 'Sourav Das' , age : 27 , profession : 'Software'}));
 
-app.use(express.static(__dirname + '/public'));
-app.set('view engine',hbs);
-app.get('/',(request,response) =>{response.render('about.hbs',{Title : 'tempTitle' ,Para : 'some rando para here'})});
-app.listen(3000);
+app.use(express.static(__dirname + '/public')); // dir to load static content from
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine',hbs); // setting the view engine to HBS
+hbs.registerHelper('getCapital',(text)=> text.toUpperCase());
+app.get('/about',(request,response) =>{
+                                      response.render('about.hbs',{Title : 'About Title' ,Para : 'About Page'
+                                      ,FooterData : `Copyright by Sourav Das @ ${(new Date()).getFullYear()}`
+                                      })
+                                      });
+
+app.get('/',(request,response) =>    {
+                                      response.render('home.hbs',{Title : 'Home Page', Para : 'some random body here',                                      ra : 'Some Body here',
+                                      FooterData : `Copyright by Sourav Das @ ${(new Date()).getFullYear()}`
+                                      })
+                                      });
+
+app.listen(4000);
